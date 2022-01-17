4ad83310e06f8cc16f34848c663e6e394fa4989f
irynastsiarzhanava <iryna.stsiarzhanava@gmail.com>
2022-01-17 12:53:42 +0100

    feat: add components and angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AddTodoComponent } from './add-todo/add-todo.component';
+import { RouterModule } from '@angular/router';
+import { ROUTES } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddTodoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(ROUTES),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
